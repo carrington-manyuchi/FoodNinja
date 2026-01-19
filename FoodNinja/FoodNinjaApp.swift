9046d46a8247e9a95acc4e22ccd8b814b2193bd9
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2026-01-19 17:44:07 +0200

    Initial Commit

---
+++ FoodNinja/FoodNinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodNinjaApp.swift
+//  FoodNinja
+//
+//  Created by Manyuchi, Carrington C on 2026/01/19.
+//
+
+import SwiftUI
+
+@main
+struct FoodNinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
